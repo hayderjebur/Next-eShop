95f7c58e415180be6a52a14c1d5f16b357716018
Hayder Jebur <hayderjabur1983@gmail.com>
2021-09-03 15:43:35 -0400

    add JWT

--- pages/login.js
+++ pages/login.js
@@ -13,9 +13,23 @@ import useStyles from '../utils/styles';
 
 export default function Login() {
   const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post('/api/users/login', {
+        email,
+        password,
+      });
+      alert('succss login');
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message);
+    }
+  };
   return (
     <Layout title='Login'>
-      <form className={classes.form}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component='h1' variant='h1'>
           Login
         </Typography>
@@ -27,6 +41,7 @@ export default function Login() {
               id='email'
               label='Email'
               inputProps={{ type: 'email' }}
+              onChange={(e) => setEmail(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
@@ -36,6 +51,7 @@ export default function Login() {
               id='password'
               label='Password'
               inputProps={{ type: 'password' }}
+              onChange={(e) => setPassword(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
